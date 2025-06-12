47f47471b99a67b91b4ba2db868ce461a5b4ac3a
leeyugyeong <uniend5959@gmail.com>
2025-06-12 17:35:21 +0900

    컬러시스템/폰트시스템등록

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import localFont from "next/font/local";
+
+export const nanumSquare = localFont({
+  src: [
+    {
+      path: "../public/fonts/NanumSquareR.ttf",
+      weight: "400", 
+    },
+    {
+      path: "../public/fonts/NanumSquareB.ttf",
+      weight: "700", 
+    },
+  ],
+  variable: "--font-nanum-square", 
+});
+
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <div className={`${nanumSquare.variable}`}>
+      <Component {...pageProps} />;
+    </div>
+    
+    </>
+  )
 }
